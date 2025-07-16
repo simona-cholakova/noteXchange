924227e4c0b0aeb2d28b39496b3ca294b5e45e62
Simona Cholakova <89231126@student.upr.si>
2025-07-17 01:04:55 +0200

    Initial setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get("/getData", (req, res) => (
+    res.send("Hello")
+))
+
+app.listen(5000, () => console.log("App is running!"));
